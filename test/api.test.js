443343b2fa98ef7bef24e0af03d5f136fe594b91
JuaniiEtche <juanietcheverry8@gmail.com>
2023-05-27 20:25:12 -0300

    Se agregó test para comprobar si responde con status 200 si se recibe algun argumento con decimales

--- test/api.test.js
+++ test/api.test.js
@@ -18,3 +18,15 @@ describe("API substract", () => {
 
     })
 })
+
+describe("API multiply", () => {
+    test("Deberia responder con un status 200 si como argumento se recibe algun numero con decimales", async () => {
+        const app = await api.build()
+
+        const res=await request(app).get('/api/v1/mul/2.5/1')
+            .expect(200)
+            .expect('Content-Type', "application/json; charset=utf-8")
+
+        expect(res.body.result).toEqual(2.5);
+    })
+})
