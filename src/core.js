2e2d36c9b6a95a5d732215e434606805f9db1268
MatiBatista <matiasomarbatista1@gmail.com>
2023-05-28 16:54:11 -0300

    Se agrego funcion decimal a binario

--- src/core.js
+++ src/core.js
@@ -42,11 +42,20 @@ function sqrt(a) {
 	}
 	return Math.sqrt(a); 
 }
+
+function decimalABinario(a) {
+    if(isNaN(a)){
+        throw Error("El parametro ingresado debe ser un numero");
+    }
+    return a.toString(2);
+}
+
 export default {
     add: add,
     sub: subtract,
     mul: multiply,
     div: divide,
     pow: pow,
-	sqrt: sqrt
+	sqrt: sqrt,
+	bin: decimalABinario
 }
