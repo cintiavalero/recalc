3cbf8622f7cf53ba7d622bfcad5b1e30287abb2f
JuaniiEtche <juanietcheverry8@gmail.com>
2023-06-14 20:02:41 -0300

    feature: Se agregó test e2e para testear la funcionalidad de multiplicar

--- test/ui.test.js
+++ test/ui.test.js
@@ -48,4 +48,39 @@ test.describe('test', () => {
     expect(historyEntry.secondArg).toEqual(9)
     expect(historyEntry.result).toEqual(70)
   });
+
+  
+  test('Deberia poder realizar una multiplicación', async ({ page }) => {
+
+    await page.goto('./');
+
+    await page.getByRole('button', { name: '2' }).click()
+    await page.getByRole('button', { name: '0' }).click()
+    await page.getByRole('button', { name: '*' }).click()
+    await page.getByRole('button', { name: '2' }).click()
+
+    const [response] = await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/api/v1/mul/')),
+      page.getByRole('button', { name: '=' }).click()
+    ]);
+
+    const { result } = await response.json();
+    expect(result).toBe(40);
+
+    await expect(page.getByTestId('display')).toHaveValue(/40/)
+
+    const operation = await Operation.findOne({
+      where: {
+        name: "MUL"
+      }
+    });
+
+    const historyEntry = await History.findOne({
+      where: { OperationId: operation.id }
+    })
+
+    expect(historyEntry.firstArg).toEqual(20)
+    expect(historyEntry.secondArg).toEqual(2)
+    expect(historyEntry.result).toEqual(40)
+  });
 })
