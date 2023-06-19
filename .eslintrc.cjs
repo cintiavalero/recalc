2a7fb8922b460a767fd389eb7d51633d5479f7db
cintiavalero <cintiavalero2001@gmail.com>
2023-06-18 21:41:08 -0300

    Revert "Se cambiaron las rules a 1, para solucionar warnings"
    This reverts commit 33e4cf32eae8e64616c03b7b1936c67a88d206b8.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,15 +9,15 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
-        "no-unused-vars": 1,
-        "no-undef": 1,
-        "no-else-return": 1,
-        "no-empty-function": 1,
-        "no-eq-null": 1,
-        "eqeqeq": 1,
-        "dot-notation": 1,
-        "curly": 1,
-        "complexity": 1
+        "no-unused-vars": 2,
+        "no-undef": 2,
+        "no-else-return": 2,
+        "no-empty-function": 2,
+        "no-eq-null": 2,
+        "eqeqeq": 2,
+        "dot-notation": 2,
+        "curly": 2,
+        "complexity": 2
     },
     "overrides": [
         {
